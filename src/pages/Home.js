48fc1f4f6935376dc794d7b6014f2dcf8d920777
radovanrasha <kking1755@gmail.com>
2025-01-05 21:42:44 +0100

    Added banners to homepage and animations, adjusted css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import memorybanner from "../assets/memory.png";
+import battleshipbanner from "../assets/battleship.jpg";
 
 const Home = () => {
   return (
     <div className="home-container">
       <Link to="/memory" className="memory-type-link">
-        <button className="memory-type-button">
+        <img
+          className="game-banner-home image-slide"
+          src={memorybanner}
+          alt="Profile"
+        />
+        <button className="memory-type-button button-slide">
           <span>Memory game</span>
         </button>
       </Link>
       <Link to="/battleship" className="memory-type-link">
-        <button className="memory-type-button">
+        <img
+          className="game-banner-home image-slide"
+          src={battleshipbanner}
+          alt="Profile"
+        />
+        <button className="memory-type-button button-slide">
           <span>Battleship game</span>
         </button>
       </Link>
