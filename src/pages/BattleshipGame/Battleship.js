4bbd6b4489032a214572e2c8107e56155c1dcdeb
radovanrasha <kking1755@gmail.com>
2025-01-05 20:58:01 +0100

    Game banners

--- src/pages/BattleshipGame/Battleship.js
+++ src/pages/BattleshipGame/Battleship.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Button } from "antd";
 import { Link } from "react-router-dom";
+import banner from "../../assets/battleship.jpg";
 
 const Memory = () => {
   return (
     <div className="memory-solo-container">
+      <img className="game-banner" src={banner} alt="Profile" />
       <div className="memory-game-row">
         <Link to="/battleship-multiplayer" className="memory-type-link">
           <button className="memory-type-button">
--- src/pages/MemoryGame/Memory.js
+++ src/pages/MemoryGame/Memory.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { Button } from "antd";
 import { Link } from "react-router-dom";
+import banner from "../../assets/memory.png";
 
 const Memory = () => {
   return (
     <div className="memory-solo-container">
+      <img className="game-banner" src={banner} alt="Profile" />
       <div className="memory-game-row">
         <Link to="/memory-sologame" className="memory-type-link">
           <button className="memory-type-button left-button">
